94c944c46e4780ecf245b3a93658a63a78327c5f
plusCubed <plusCubed@users.noreply.github.com>
2014-04-10 20:39:57 -0700

    Should be with last commit (ACRA bug reporting) - forgot to commit this file

---
+++ app/src/main/java/com/pluscubed/plustimer/PlusTimer.java
@@ -0,0 +1,33 @@
+package com.pluscubed.plustimer;
+
+import android.app.Application;
+
+import org.acra.ACRA;
+import org.acra.ACRAConfiguration;
+
+/**
+ * ACRA Bug Reporting Application class
+ */
+public class PlusTimer extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        /*
+         * Code from http://commonsware.com/blog/2013/01/28/what-not-to-put-in-your-repo.html
+         * by Mark Murphy
+         * Copyright © 2010-2013 CommonsWare, LLC
+         * Used under the Creative Commons Attribution Non-Commercial Share Alike 3.0 License: http://creativecommons.org/licenses/by-nc-sa/3.0/
+         *
+         * Changed the string reference name
+         */
+
+        if (!BuildConfig.DEBUG) {
+            ACRAConfiguration config=ACRA.getNewDefaultConfig(this);
+            config.setFormUri(getString(R.string.bugsense_form_uri));
+            ACRA.setConfig(config);
+            ACRA.init(this);
+        }
+    }
+}
