a053d24deb7ed057646195296f31d2d0065b6a59
plusCubed <plusCubed@users.noreply.github.com>
2014-04-11 18:32:04 -0700

    Change ACRA to dialog mode

--- app/src/main/java/com/pluscubed/plustimer/PlusTimer.java
+++ app/src/main/java/com/pluscubed/plustimer/PlusTimer.java
@@ -4,10 +4,15 @@ import android.app.Application;
 
 import org.acra.ACRA;
 import org.acra.ACRAConfiguration;
+import org.acra.ACRAConfigurationException;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
 
 /**
  * ACRA Bug Reporting Application class
  */
+
+@ReportsCrashes(formKey = "")
 public class PlusTimer extends Application {
 
     @Override
@@ -20,14 +25,22 @@ public class PlusTimer extends Application {
          * Copyright © 2010-2013 CommonsWare, LLC
          * Used under the Creative Commons Attribution Non-Commercial Share Alike 3.0 License: http://creativecommons.org/licenses/by-nc-sa/3.0/
          *
-         * Changed the string reference name
+         * Changed the string reference name & added dialog settings
          */
-/*
+
         if (!BuildConfig.DEBUG) {
             ACRAConfiguration config=ACRA.getNewDefaultConfig(this);
             config.setFormUri(getString(R.string.bugsense_form_uri));
+            try {
+                config.setMode(ReportingInteractionMode.DIALOG);
+            } catch (ACRAConfigurationException e) {
+                e.printStackTrace();
+            }
+            config.setResDialogText(R.string.acra_crash_dialog_text);
+            config.setResDialogCommentPrompt(R.string.acra_crash_dialog_prompt);
+            config.setResDialogTitle(R.string.acra_crash_dialog_title);
             ACRA.setConfig(config);
             ACRA.init(this);
-        }*/
+        }
     }
 }
