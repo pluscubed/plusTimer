5af4f506b0186e455a7500c730c56657e37cb4a5
plusCubed <plusCubed@users.noreply.github.com>
2014-10-24 20:06:29 -0700

    Prevent invalid characters in solve dialog time entering

--- app/src/main/java/com/pluscubed/plustimer/ui/SettingsActivity.java
+++ app/src/main/java/com/pluscubed/plustimer/ui/SettingsActivity.java
@@ -11,7 +11,6 @@ import android.preference.EditTextPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
-import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
 import android.support.annotation.NonNull;
 import android.support.v4.app.NavUtils;
