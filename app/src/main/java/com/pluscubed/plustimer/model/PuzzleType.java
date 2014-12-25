pluscubed/plusTimer

commit dc8bf38bc091c368e780903ed6aaf8499fefa995

Fix notifying on deleting solves

app/src/main/java/com/pluscubed/plustimer/model/PuzzleType.java
@@ -107,14 +107,14 @@ public enum PuzzleType {
     }
 
     public static void setCurrent(PuzzleType type, Context context) {
-        if (type != sCurrentPuzzleType) {
-            sCurrentPuzzleType = type;
-            notifyPuzzleTypeChanged();
-        }
         SharedPreferences defaultSharedPreferences = PreferenceManager
                 .getDefaultSharedPreferences(context);
         defaultSharedPreferences.edit().putString(PREF_CURRENT_PUZZLETYPE,
                 sCurrentPuzzleType.name()).apply();
+        if (type != sCurrentPuzzleType) {
+            sCurrentPuzzleType = type;
+            notifyPuzzleTypeChanged();
+        }
     }
 
     public static List<PuzzleType> valuesExcludeDisabled() {

app/src/main/java/com/pluscubed/plustimer/model/Session.java
@@ -216,8 +216,9 @@ public class Session {
     }
 
     public void deleteSolve(Solve i) {
-        notifySolveDeleted(mSolves.indexOf(i));
+        int position = mSolves.indexOf(i);
         mSolves.remove(i);
+        notifySolveDeleted(position);
     }
 
     public String toString(Context context, String puzzleTypeName,

app/src/main/java/com/pluscubed/plustimer/ui/HistorySessionListFragment.java
@@ -280,8 +280,8 @@ public class HistorySessionListFragment extends ListFragment {
                     }
                     try {
                         GraphViewSeries averageSeries = new GraphViewSeries(
-                                String.format(getString(R.string.bao),bestAverageMatrix.keyAt(i)),
-                                new GraphViewSeries.GraphViewSeriesStyle(lineColor,Util.convertDpToPx(getActivity(), 2)),
+                                String.format(getString(R.string.bao), bestAverageMatrix.keyAt(i)),
+                                new GraphViewSeries.GraphViewSeriesStyle(lineColor, Util.convertDpToPx(getActivity(), 2)),
                                 bestTimesDataArray);
                         bestAverageGraphViewSeries.add(averageSeries);
                     } catch (final IllegalArgumentException e) {
@@ -356,12 +356,12 @@ public class HistorySessionListFragment extends ListFragment {
                         bestSolvesTimes.valueAt(i));
             }
 
-            GraphViewSeries bestTimesSeries=null;
+            GraphViewSeries bestTimesSeries = null;
             try {
-                 bestTimesSeries= new GraphViewSeries(getString(R.string.best_times),
-                        new GraphViewSeries.GraphViewSeriesStyle(Color.BLUE,Util.convertDpToPx(getActivity(), 2)),
+                bestTimesSeries = new GraphViewSeries(getString(R.string.best_times),
+                        new GraphViewSeries.GraphViewSeriesStyle(Color.BLUE, Util.convertDpToPx(getActivity(), 2)),
                         bestTimesDataArray);
-            }catch (final IllegalArgumentException e) {
+            } catch (final IllegalArgumentException e) {
                 //TODO: Remove once History bug fixed
                 if (BuildConfig.USE_CRASHLYTICS) Crashlytics.logException(e);
                 new MaterialDialog.Builder(getActivity())

commit 5f1931e107691931abc76e9015e529fcd28b6c8b

Fix saving current session (broken in commit dc8bf38)

app/src/main/java/com/pluscubed/plustimer/model/PuzzleType.java
@@ -110,7 +110,7 @@ public enum PuzzleType {
         SharedPreferences defaultSharedPreferences = PreferenceManager
                 .getDefaultSharedPreferences(context);
         defaultSharedPreferences.edit().putString(PREF_CURRENT_PUZZLETYPE,
-                sCurrentPuzzleType.name()).apply();
+                type.name()).apply();
         if (type != sCurrentPuzzleType) {
             sCurrentPuzzleType = type;
             notifyPuzzleTypeChanged();